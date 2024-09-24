878cdcfce88fd52524d59fdcaaaa65c8c3264d97
L1tDeV <113245394+L1tDeV@users.noreply.github.com>
2024-09-24 20:03:34 +0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import './globals.scss'
 import Head from 'next/head'
 
 export const metadata = {
-  title: 'SmaF1',
+  title: 'L1tDeV',
   description: 'This site-portfolio is written in Next.js',
 }
 
